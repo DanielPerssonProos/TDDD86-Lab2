34bf641b2715c3c4eb2ea182ac9a01cc3bd09208
Sebastian Lindmark <sebbe.lindmark@gmail.com>
2015-09-22 13:42:31 +0200

    added new cleaned up evilhangman

--- evilhangman/src/evilhangman.cpp
+++ evilhangman/src/evilhangman.cpp
@@ -3,17 +3,27 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
-const string dictFilePath = "/Users/Daniel/Documents/Programmering/TDDD86/Lab2/evilhangman/res/dictionary.txt";
+//const string dictFilePath = "/Users/Daniel/Documents/Programmering/TDDD86/Lab2/evilhangman/res/dictionary.txt";
+const string dictFilePath = "dictionary.txt";
+
+const string ANSWER_YES = "yes";
+const string ANSWER_NO = "no";
 
 map<int, vector<string> > loadDictionary() {
     map<int, vector<string> > dictMap;
     ifstream stream;
     string word;
     stream.open(dictFilePath);
+
+    if(!stream.is_open()){
+        cout << "Unable to find file" << endl;
+    }
+
     while (!stream.eof()) {
         stream >> word;
         dictMap[word.length()].push_back(word);
@@ -27,32 +37,30 @@ pair<string,vector<string> > divideWordFamilies(char guessedLetter,int wordLengt
 
     for(string word : words){
         string keyString = wordToGuess;
-        occurences = count(word.begin(),word.end(),guessedLetter);
+        occurences = std::count(word.begin(),word.end(),guessedLetter);
 
         if(occurences == 1){
             int letterIndex = word.find(guessedLetter);
             keyString.at(letterIndex) = guessedLetter;
+
         }else if(occurences == 2){
             int firstLetterIndex = word.find(guessedLetter);
             int lastLetterIndex = word.find_last_of(guessedLetter);
-
             keyString.at(firstLetterIndex) = guessedLetter;
             keyString.at(lastLetterIndex) = guessedLetter;
-        }else if(occurences > 2){
 
+        }else if(occurences > 2){
             for(int i = 0; i < wordLength;++i){
                 if(word.at(i) == guessedLetter){
                     keyString.at(i) = guessedLetter;
                 }
             }
-
         }
-
         families[keyString].push_back(word);
     }
 
 
-    int sizeOfBiggest = 0;
+    unsigned int sizeOfBiggest = 0;
     pair<string, vector<string> > biggestFamily;
 
     for(auto iterator = families.begin(); iterator != families.end(); ++iterator){
@@ -68,9 +76,8 @@ void runGame(bool& showWordCount, int& nmGuesses, int wordLength, vector<string>
     string prevGuessedLetters = "";
     string wordToGuess = string(wordLength, '-');
     char guessedLetter;
-
-
     bool gameEnded = false;
+
     while(!gameEnded) {
 
         int wordsLeft = words.size();
@@ -95,6 +102,7 @@ void runGame(bool& showWordCount, int& nmGuesses, int wordLength, vector<string>
         if (family.first == wordToGuess) {
             --nmGuesses;
         }
+
         wordToGuess = family.first;
         words = family.second;
         if (nmGuesses == 0) {
@@ -113,35 +121,53 @@ void runGame(bool& showWordCount, int& nmGuesses, int wordLength, vector<string>
 }
 
 
-
-
 int main() {
-    int nmGuesses = -1;
-    int wordLength= -1;
-    string placeHolder = "";
-    bool showWordCount = false;
     cout << "Welcome to Hangman." << endl;
+
+    bool playAgain = true;
     map<int, vector<string> > dictMap = loadDictionary();
-    while (dictMap.find(wordLength) == dictMap.end()) {
-        cout << "Please enter the valid length of a word: ";
-        cin >> wordLength;
-    }
-    while (nmGuesses <= 0) {
-        cout << "Type the number of guesses that you want: ";
-        cin >> nmGuesses;
-    }
-    while (placeHolder != "y" && placeHolder != "n") {
-        cout << "Do you want to see the number of words left after each guess? (y/n) ";
-        cin >> placeHolder;
-    }
-    if (placeHolder == "y") {
-        showWordCount = true;
-    }
-    runGame(showWordCount, nmGuesses, wordLength, dictMap[wordLength]);
 
+    while(playAgain){
 
+        /* Get the length of the word to guess */
 
-    // TODO: Finish the program!
+        int wordLength= -1;
+        while (dictMap.find(wordLength) == dictMap.end()) {
+            cout << "Please enter the valid length of a word: ";
+            cin >> wordLength;
+        }
+
+        /* Get number of guesses */
+
+        int nmGuesses = -1;
+        while (nmGuesses <= 0) {
+            cout << "Type the number of guesses that you want: ";
+            cin >> nmGuesses;
+        }
+
+        /* Get if user wants to see number of words left */
+
+        bool showWordCount = false;
+        string inputUserChoice = "";
+        while (inputUserChoice != ANSWER_YES && inputUserChoice != ANSWER_NO) {
+            cout << "Do you want to see the number of words left after each guess? (" << ANSWER_YES << "/" << ANSWER_NO << ") ";
+            cin >> inputUserChoice;
+        }
+        if (inputUserChoice == ANSWER_YES) {
+            showWordCount = true;
+        }
+
+        /* Run game */
+
+        runGame(showWordCount, nmGuesses, wordLength, dictMap[wordLength]);
+
+        /* Get if user wants to play again */
+
+        string playAgainInput = "";
+        cout <<  endl << "Do you want to play again? (" << ANSWER_YES << "/" << ANSWER_NO << ") ";
+        cin >> playAgainInput;
+        playAgain = (playAgainInput == ANSWER_YES);
+    }
 
     return 0;
 }
